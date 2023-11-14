3d76692da5b845bb9566f2cd1f5ed29889109b67
XavierDebuf <x.debuf@gmail.com>
2023-11-14 10:49:26 +0100

    upload doc OK, cid IPFS ok, connect to Mongodb OK, buffer problem

--- back/middleware/connectDb.js
+++ back/middleware/connectDb.js
@@ -3,18 +3,24 @@ import pkg2 from 'mongoose';
 const { mongoose } = pkg2;
 let dbConnection;
 export async function connectToDb(cb) {
-    mongoose.connect("mongodb://0.0.0.0:27017/poc_hash_storage",{
+    MongoClient.connect("mongodb://0.0.0.0:27017/poc_hash_storage")
+    /*,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
-      })
-        .then((client) => {
+      }).connection;*/
+       .then((client) => {
             dbConnection = client.db();
+            console.log('client connecté')
             return cb();
         })
         .catch((err) => {
             console.log(err);
             return cb(err);
-        });
+        });/*
+        mongoose.on('error', function(err) { console.log(err.message); });
+        mongoose.once('open', function() {
+        console.log("mongodb connection open");*/
+        //});
 }
 export function getDb() { return dbConnection; }
